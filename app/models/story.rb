f6cd32da3ac4c138cf3c82b81e51705a7d322d9b
cdeliens <cdeliens@gmail.com>
2013-04-18 14:49:08 -0600

    test green

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,12 @@
 class Story < ActiveRecord::Base
   attr_accessible :description, :title
+
+  after_create :default_title
+
+  private
+
+  def default_title
+    self.title =  "Default Message"
+    self.save
+  end
 end
