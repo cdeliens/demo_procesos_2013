2f12db6aecc5e7d273d590022879c05790e230ec
cdeliens <cdeliens@gmail.com>
2013-04-18 14:57:20 -0600

    default description

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,4 +8,8 @@ class Story < ActiveRecord::Base
   def default_title
     self.update_attributes(:title => "Default Message")
   end
+  
+  def default_description
+    self.update_attributes(:description => "Default Description")
+  end
 end
