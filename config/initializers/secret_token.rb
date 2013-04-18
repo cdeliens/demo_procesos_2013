cec50071b519a3a47b1f78f6c252b3aea92d4dee
cdeliens <cdeliens@gmail.com>
2013-04-18 14:15:44 -0600

    commit inicial con las aplicaciones basicas para la administracion de la configuracion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoProcesos2013::Application.config.secret_token = '28a92d615f02a3c63fa972466fc439edf3270a93e2f74a89f835db02b983954dcd6064612ee56b396026ac3e2626bde3d386091ab9c0be4f3dec183f71f19efa'
